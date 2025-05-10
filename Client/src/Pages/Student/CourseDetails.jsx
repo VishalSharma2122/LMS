73f52dbaccfd63ecdf0dd4cc56adab88ec710f1d
VishalSharma2122 <vishalsharma21221358@gmail.com>
2025-05-10 23:22:34 +0530

    add

--- Client/src/Pages/Student/CourseDetails.jsx
+++ Client/src/Pages/Student/CourseDetails.jsx
@@ -1,20 +1,24 @@
 import { assets } from "@/assets/assets";
+import Footer from "@/Components/Student/Footer";
 import Loading from "@/Components/Student/Loading";
 import { AppContext } from "@/Context/AppContext";
 import humanizeDuration from "humanize-duration";
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
 
 function CourseDetails() {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSections] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
+  const [playerData, setPlayerData] = useState(null);
   const {
     allCourses,
     calculateRating,
     calculateChapterTime,
     calculateCourseDuration,
-    calculateNoOfLecture, 
+    calculateNoOfLecture,
     currency,
   } = useContext(AppContext);
 
@@ -39,7 +43,7 @@ function CourseDetails() {
     setOpenSections((prev) => ({
       ...prev,
       [index]: !prev[index],
-    }));    
+    }));
   };
 
   return courseData ? (
@@ -134,7 +138,16 @@ function CourseDetails() {
                             <p>{lecture.lectureTitle}</p>
                             <div className="flex  gap-2">
                               {lecture.isPreviewFree && (
-                                <p className="text-blue-500 cursor-pointer">
+                                <p
+                                  onClick={() =>
+                                    setPlayerData({
+                                      videoId: lecture.lectureUrl
+                                        .split("/")
+                                        .pop(),
+                                    })
+                                  }
+                                  className="text-blue-500 cursor-pointer"
+                                >
                                   Preview
                                 </p>
                               )}
@@ -169,23 +182,85 @@ function CourseDetails() {
 
         {/* Right Column */}
         <div className=" max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none bg-white min-w-[30px] sm:min-w-[420px] ">
-          <img src={courseData.courseThumbnail} alt="Thumbnail" />
+          {
+            playerData ? (
+                <YouTube
+                  videoId={playerData.videoId}
+                  opts={{ playerVars: { autoplay: 1 } }}
+                  iframeClassName="w-full aspect-video"
+                />
+              ) : (
+                <img
+                  className="w-3.5"
+                  src={assets.time_left_clock_icon}
+                  alt=" tijme left icon"
+                />
+          )}
           <div className="p-5 ">
             <div className="flex items-center gap-2 ">
-              <img
-                className="w-3.5"
-                src={assets.time_left_clock_icon}
-                alt=" tijme left icon"
-              />
-              <p className=" text-red-500"> <span className="font-medium ">5 days</span> left at this price
+                <img
+                  className="w-3.5"
+                  src={assets.time_left_clock_icon}
+                  alt=" tijme left icon"
+                />
+              <p className=" text-red-500">
+                {" "}
+                <span className="font-medium ">5 days</span> left at this price
               </p>
             </div>
-            <div>
-              <p></p>
+            <div className="flex items-center gap-3 pt-2">
+              <p className="text-gray-800 md: text-4xl text-2xl font-semibold">
+                {currency}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg text-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg text-gray-500">{courseData.discount}%</p>
+            </div>
+            <div className="flex items-center text-sm md:text-default gap-4 text-gray-500 pt-2 md:pt-4">
+              <div className="flex items-center gap-1">
+                <img src={assets.star} alt="star icon " />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="clock icon " />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="clock icon " />
+                <p>{calculateNoOfLecture(courseData)} lessons</p>
+              </div>
+            </div>
+            <button className="md:mt-6 mt-4 w-full rounded bg-blue-600 text-white font-medium">
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+            <div className="pt-6 ">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in this course?
+              </p>
+              <ul className=" ml-4 list-disc md:text-default pt-2 text-sm text-gray-500">
+                <li>Downloadable resources</li>
+                <li>Full lifetime access</li>
+                <li>Access on mobile and TV</li>
+                <li>Certificate of completion</li>
+                <li>Assignments</li>
+                <li>Quizzes</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
